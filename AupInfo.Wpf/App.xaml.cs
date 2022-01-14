a5202782d8f751bc03d2a3e2442fa1f3da56e9fd
karoterra <karoterra2008@gmail.com>
2022-01-14 19:45:40 +0900

    :sparkles: feat: WPFプロジェクトと空のメインウィンドウを追加

---
+++ AupInfo.Wpf/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Windows;
+using Prism.Ioc;
+using AupInfo.Wpf.Views;
+
+namespace AupInfo.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ AupInfo.Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Reactive.Disposables;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace AupInfo.Wpf.ViewModels
+{
+    public class MainWindowViewModel : BindableBase, IDisposable
+    {
+        private readonly CompositeDisposable disposables = new();
+
+        public ReactivePropertySlim<string> Title { get; }
+
+        public MainWindowViewModel()
+        {
+            Title = new ReactivePropertySlim<string>("AupInfo").AddTo(disposables);
+        }
+
+        #region IDisposable
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    disposables.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
